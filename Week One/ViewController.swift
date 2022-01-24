4f2b8a3fdb6db961e15b67abe00f5c823b39e679
Daniel Park <parkant@bc.edu>
2022-01-24 18:40:02 -0500

    class commit

--- Week One/ViewController.swift
+++ Week One/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var sayitButton: UIButton!
+    let arrayofMessage = ["You are Awesome!", "You did it!",
+                        "Okay okay", "Let's go!", "Nice...", "wOoohohoh", "This class is fun!", "Take me out to a ball game", "amaseamsauhamamakush"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("app loaded on ", Date())
+        messageLabel.isHidden = true
     }
-
-
+    @IBAction func messageButton(_ sender: Any) {
+        print("Message Button was pressed")
+        
+    }
+    
+    @IBAction func sayitButton(_ sender: Any) {
+        print("👉🏽The say it button was pressed", Date())
+        messageLabel.text = arrayofMessage.randomElement()
+        messageLabel.isHidden = false
+        if (messageLabel.text == "You are Awesome!"){
+            sayitButton.isHidden = true
+        }
+    }
+    
 }
 
