449055ad14c617a1cbfbb5e19305d4bb5a35a27e
Daniel Park <parkant@bc.edu>
2022-01-30 01:53:14 -0500

    ch 2.6

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//  Directory Update ////////////
+//
+//  Created by Danny Park on 1/23/22. UPDATE
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😂 The message button was pressed!")
+        messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "img0")
+    }
+}
+
